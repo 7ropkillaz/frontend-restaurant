7f07af5e1a794df2d5ed18259670b0cbea6ddc8c
7ropkillaz <kusainovdaniyar@gmail.com>
2020-04-28 17:02:45 +0400

    Back and front binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import {SelectionModel} from '@angular/cdk/collections';
 import {MatTable, MatTableDataSource} from '@angular/material/table';
-import {FormsModule} from '@angular/forms';
+import {FormControl, FormsModule} from '@angular/forms';
 import {Dish} from '../models/Dish';
 import Table = WebAssembly.Table;
+import {DataService} from '../service/data.service';
+import {HttpService} from '../service/http.service';
+import {error} from 'util';
 
 
 /**
@@ -13,18 +16,20 @@ import Table = WebAssembly.Table;
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent implements OnInit {
   @ViewChild("tableOfDishes", {static: false})
   tableOfDishes: MatTable<any>;
   listDishes: Dish[] = [];
-
+  myControl = new FormControl();
+  options: string[]=[];
   columnsToDisplay = ['id', 'name', 'price', 'category'];
   id: number;
   name: string;
   price: number;
   category: string;
-
+  constructor(private dataService: DataService, private httpService:HttpService){}
 
 
   ngOnInit(): void {
@@ -32,7 +37,13 @@ export class AppComponent implements OnInit {
     let dataFromServer: Dish[] = [
       {id: 1, name: 'purump', category: 'dumdump', price:281 }
     ];
+    this.options=this.dataService.getData();
     this.listDishes=dataFromServer;
+    this.httpService.getData().subscribe((data:Dish[]) => {
+      console.log(data);
+      this.listDishes=data
+    });
+
   }
 
   removeDishById() {
@@ -40,6 +51,11 @@ export class AppComponent implements OnInit {
       if(this.listDishes[i].id==this.id){
         this.listDishes.splice(i,1);
         console.log(this.id);
+        this.httpService.deleteData(this.id.toString()).subscribe(
+          (data:Dish)=>{
+            console.log(data);
+          }
+        )
         this.tableOfDishes.renderRows();
       }
     }
@@ -49,8 +65,14 @@ export class AppComponent implements OnInit {
     let dish: Dish={id:this.id, name:this.name, price:this.price, category:this.category };
     if(this.id!=null){
     this.listDishes.push(dish);
-    console.log(this.listDishes);
+    this.dataService.addData(this.category);
     this.tableOfDishes.renderRows();
+
+    this.httpService.addData(dish).subscribe(
+      ()=>{console.log("OK!")},
+      error1 => {alert("ERROR!!!")}
+    );
+    console.log(this.listDishes);
     }
   }
 
@@ -60,6 +82,7 @@ export class AppComponent implements OnInit {
         let temp: Dish = {id: this.id, name: this.name, price: this.price, category: this.category};
         this.listDishes.splice(i, 1);
         this.listDishes.push(temp);
+        this.httpService.updateData(temp);
         console.log(this.listDishes);
         this.tableOfDishes.renderRows();
       }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTableModule} from '@angular/material/table';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {DataService} from '../service/data.service';
+import {HttpService} from '../service/http.service';
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
@@ -16,10 +20,12 @@ import {FormsModule} from '@angular/forms';
     BrowserAnimationsModule,
     MatTableModule,
     MatCheckboxModule,
-    FormsModule
-
+    FormsModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataService, HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
